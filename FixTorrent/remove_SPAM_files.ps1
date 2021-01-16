cbf147e08d764fc271d6bc1cb3c641b172812253
hmaster20 <hmaster20@gmail.com>
2021-01-16 19:28:27 +0300

    Cleansing data downloaded from the torrent tracker SHAREWOOD from spam

---
+++ FixTorrent/remove_SPAM_files.ps1
@@ -0,0 +1,7 @@
+﻿cd "D:\Download"
+
+$file1 = "*Прочти перед изучением!.docx"
+$file2 = "SHAREWOOD_ZERKALO_COM_90000_курсов_на_нашем_форуме!.url"
+
+Get-ChildItem .\* -Include $file1,$file2 -Recurse | Remove-Item -Force
+Get-ChildItem .\* -Include $file1,$file2 -Recurse
---
+++ FixTorrent/remove_SPAM_prefix.ps1
@@ -0,0 +1,4 @@
+﻿cd "D:\Download"
+
+dir -r | Rename-Item -NewName { $_.name -replace "\[SW.BAND\]","" }
+dir -r | Rename-Item -NewName { $_.name.Trim() }
