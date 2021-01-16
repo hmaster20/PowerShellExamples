367eb6ba5c7a9ef7ee74d6d02f7b74a0d9dcb7aa
hmaster20 <hmaster20@gmail.com>
2021-01-16 19:27:42 +0300

    Windows.edb file servicing examples

---
+++ WindowsEDB/DefragWindowsEDB.ps1
@@ -0,0 +1,12 @@
+﻿# esentutl.exe (Extensible Storage Engine Utilities) —  утилиты обслуживания таких баз EDB 
+# Если при выполнении команды esentutl появляется ошибка:
+# Operation terminated with error -1213 (JET_errPageSizeMismatch, The database page size does not match the engine) after 10.125 seconds
+# Это означает что ваша система 64 битная и для выполнения дефрагментации нужно использовать x32 версию esentutl. 
+# В этом случае третья команда будет выглядеть так:
+# "C:\Windows\SysWOW64\esentutl.exe" /d %AllUsersProfile%\Microsoft\Search\Data\Applications\Windows\Windows.edb
+#
+sc config wsearch start=disabled
+sc stop wsearch
+esentutl.exe /d %ProgramData%\Microsoft\Search\Data\Applications\Windows\Windows.edb
+sc config wsearch start=delayed-auto
+sc start wsearch
---
+++ WindowsEDB/ReCreateWindowsEDB.ps1
@@ -0,0 +1,8 @@
+﻿# Значение параметра реестра SetupCompletedSuccessfully = 0 в ветке HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Search 
+# заставляет службу Windows Search при следующем запуске сбросить текущие настройки и пути индексирования, 
+# восстановит стандартные параметры wsearch и добавит пути для индексирования по-умолчанию.
+# 
+net stop "Windows Search"
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows Search" /v SetupCompletedSuccessfully /t REG_DWORD /d 0 /f
+del %PROGRAMDATA%\Microsoft\Search\Data\Applications\Windows\Windows.edb
+net start "Windows Search"
