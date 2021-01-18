dafdaec3b00eea4e4b97c0211cc4b6c944320374
hmaster20 <hmaster20@gmail.com>
2021-01-18 11:37:54 +0300

    files for processing media files

---
+++ Foto/FolderHelper.ps1
@@ -0,0 +1,20 @@
+﻿# Выполняем поиск каталогов и группируем их по годам
+$CurrentFolder="D:\Foto"
+$folders = Get-ChildItem $CurrentFolder\* -dir
+foreach ($folder in $folders) {
+    $folderArray=$folder.Name.Split('.')
+
+    if ($folderArray.Count -gt 2)
+    {
+        $folderYear=$folderArray[0]
+
+        $folderDest="$CurrentFolder\$folderYear\"
+
+        if(!(Test-Path $folderDest))
+        {
+            New-Item -Path $folderDest -ItemType Directory -Force | Out-Null
+        }
+
+        Move-Item $folder $folderDest -Verbose
+    }
+}
---
+++ Foto/FotoHelper.ps1
@@ -0,0 +1,16 @@
+﻿# Выполняем поиск фотографий и видеороликов на основе даты модификации и переносим их в каталоги на основе даты
+$folder="D:\Foto"
+$files = Get-ChildItem $folder\* -File
+
+foreach ($file in $files) {
+    $filedate=((Get-ChildItem $file).LastWriteTime.toString('yyyy.MM.dd'))
+
+    $folderDest="$folder\$filedate\"
+
+    if(!(Test-Path $folderDest))
+    {
+        New-Item -Path $folderDest -ItemType Directory -Force | Out-Null
+    }
+
+    Move-Item $file $folderDest -Verbose
+}
