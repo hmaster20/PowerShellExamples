d8a47f0402e99ce25659b7327adf90aef38413f9
hmaster20 <hmaster20@gmail.com>
2021-01-20 12:59:47 +0300

    Update file windows search

--- WindowsHelper/FindLargestFilesOnDisk.ps1
+++ WindowsHelper/FindLargestFilesOnDisk.ps1
@@ -1,4 +1,7 @@
-﻿# Выведем список 10 самых больших файлов.
+﻿# Выведем список 10 самых больших файлов, более 300 МБ.
+$FileSize=300 # MB
+$size=$FileSize*1024*1024
+# Вместо Get-ChildItem можно использовать псевдоним GCi
 # Ключ –r (Recurse) указывает, что необходимо рекурсивного обойти все вложенные обьекты (каталоги). 
 # При доступе к некоторым каталогам, даже с правами администратора, командлет может вернуть ошибку отказа в доступе.
 # Чтобы подавить появление таких ошибок нужен параметр -ErrorAction SilentlyContinue.
@@ -7,7 +10,15 @@
 # Чтобы избежать вывода размера файлов в байтах, преобразуем их сразу в мегабайты.
 
 Get-ChildItem C:\VBox -r -ErrorAction SilentlyContinue -Depth 3 | `
+	where-object {$_.length -gt $size} | `
+	Sort-Object -descending -property length | `
+    Select-Object -first 10 name, DirectoryName, @{Name="MB";Expression={[Math]::round($_.length / 1MB, 2)}} 
+
+# Другой вариант записи, используя псевдонимы
+
+GCi C:\VBox -r -ErrorAction SilentlyContinue -Depth 3 | `
+	where {$_.length -gt $size} | `
 	sort -descending -property length | `
     select -first 10 name, DirectoryName, @{Name="MB";Expression={[Math]::round($_.length / 1MB, 2)}} 
 
-# Полученную табличку можно преобразовать в удобную графическую форму с помощью командлета Out-GridView
+# Полученную таблицу можно преобразовать в графическую форму с помощью командлета Out-GridView
